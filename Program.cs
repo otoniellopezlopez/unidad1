b58456f5d3e57710b0e9776e8e5b2d50ec3053cf
otoniellopezlopez <54568851+otoniellopezlopez@users.noreply.github.com>
2019-09-12 23:40:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class operaciones
+    {
+        static void Main(string[] args)
+        {
+           suma();
+        }
+
+        static void suma(){
+            int n1=10, n2=89;
+            int suma=n1+n2;
+            Console.WriteLine(suma);
+        }
+    }
+}
