8c335d2f76351b655f3ce45a30e5b7082399c466
Gabrielagomezrob <54647304+Gabrielagomezrob@users.noreply.github.com>
2019-09-18 16:14:46 -0500

    Add files via upload

---
+++ Resta gabrielaProgram.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Calculadora
+{
+    class operaciones
+    {
+        static void Main(string[] args)
+        {
+           suma();
+        }
+
+        static void suma(){
+            int n1=10, n2=89;
+            int suma=n1+n2;
+            Console.WriteLine(suma);
+        }
+    }
+}
+static int Resta(){
+        {
+            int resta = n1-n2;
+            return resta;
+        }
+        
