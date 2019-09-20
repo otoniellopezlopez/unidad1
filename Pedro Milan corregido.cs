3465ce08686095c2fc26a9a4b231197d28cb96d0
PedroMilanCo <55250887+PedroMilanCo@users.noreply.github.com>
2019-09-20 16:02:26 -0500

    Add files via upload

---
+++ Pedro Milan corregido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class operaciones
+    {
+        static void Main(string[] args)
+        {
+           suma();
+           multiplicacion();
+        }
+
+         static void suma(){
+            int n1=10, n2=89;
+            int suma=n1+n2;
+            Console.WriteLine(suma);
+        }
+         static void multiplicacion(){
+            int n1=10, n2=89;
+            int multiplicacion=n1*n2;
+            Console.WriteLine(multiplicacion);
+        }    
+    }
+}
