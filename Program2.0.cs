b3785b5802cdc98889bc2738b54391c4ceb8f1e3
Gabrielagomezrob <54647304+Gabrielagomezrob@users.noreply.github.com>
2019-09-19 22:08:47 -0500

    Add files via upload

---
+++ Program2.0.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class operaciones
+    {
+        static void Main(string[] args)
+        {
+           suma();
+           resta();
+        }
+
+         static void suma(){
+            int n1=10, n2=89;
+            int suma=n1+n2;
+            Console.WriteLine(suma);
+        }
+         static void resta(){
+            int n1=10, n2=89;
+            int resta=n1-n2;
+            Console.WriteLine(resta);
+        }    
+    }
+}
